9b987ae55a3c1ba6c0b01f9571a3cb3053a3ce41
Denis Shevchenko <denis_sheff@icloud.com>
2022-12-03 11:05:40 +0900

    The task is solved

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,4 +1,30 @@
-﻿for (int i = 0; i < length; i++)
-{
+﻿
 
+string[] arrayString = new string[5] { "hello", "2", "world", ":-)", "computer science" };
+string[] newArrayString = new string[arrayString.Length];
+int maxSizeString = 3;
+
+Console.Clear();
+void CreateMaxThreeSymbolsArray(string[] collection, string[] newCollection)
+{
+	int count = 0;
+	for (int i = 0; i < collection.Length; i++)
+	{
+		if (collection[i].Length <= maxSizeString)
+		{
+			newCollection[count] = collection[i];
+			count++;
+		}
+	}
 }
+void PrintArray(string[] block)
+{
+	for (int i = 0; i < block.Length; i++)
+	{
+		Console.Write($"{block[i]} ");
+	}
+	Console.WriteLine();
+}
+
+CreateMaxThreeSymbolsArray(arrayString, newArrayString);
+PrintArray(newArrayString);
